f63b1db117d1e0a3c9dbc08b6d54d63bba2ca0bd
Blake <Blake@DESKTOP-A3TKUI6>
2019-07-09 05:35:33 -0700

    Login Dao added

--- src/com/dev/dao/JDBCImpl.java
+++ src/com/dev/dao/JDBCImpl.java
@@ -1,5 +1,11 @@
 package com.dev.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import com.dev.beans.User;
 
 public class JDBCImpl implements UserDAO {
@@ -30,7 +36,58 @@ public class JDBCImpl implements UserDAO {
 
 	@Override
 	public User login(Integer userId, String password) {
-		return null;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		User user = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			String dbUrl = "jdbc:mysql://localhost:3306/cleveridiots_db";
+			con = DriverManager.getConnection(dbUrl,"root","root");
+			String sql = "select * from users_info "
+					+ "	where user_id=? and password = ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, userId);
+			pstmt.setString(2, password);
+			rs = pstmt.executeQuery();
+			if(rs.next()){
+				user = new User();
+				user.setUserId(rs.getInt("user_id"));
+				user.setFirstName(rs.getString("firstname"));
+				user.setLastName(rs.getString("lastname"));
+				user.setPassword(rs.getString("password"));
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			if(con != null){
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			if(pstmt != null){
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			if(rs != null){
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		}
+		return user;
 	}
 
 }
